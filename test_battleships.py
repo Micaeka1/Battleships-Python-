c1852575a75e94197764bae2c241e50a55911e7b
Micaeka1 <micaelavalero1@gmail.com>
2021-01-04 14:44:06 +0000

    Tests for functions in randomly_place_all_ships

--- test_battleships.py
+++ test_battleships.py
@@ -1,48 +1,64 @@
 import pytest
 from battleships import *
     
-def test_is_sunk1():
-    s = (2, 3, False, 3, {(2,3), (3,3), (4,3)})
-    assert is_sunk(s) == True
-    t1 = (2,3,True,3,{(2,3), (2,4)})
-    assert is_sunk(t1) == False
-    t2 = (1,0,True,1,{(1,0)}
-    assert is_sunk(t2) == True
-    t3 = (5,7,False,3,{(5,7),(6,7)}
-    assert is_sunk(t3) == True
-    t4 = (8,4,True, 4, {(8,6),(8,7),(8,4)}
-    assert is_sunk(t4) == False
+s1 = (2, 2, False, 3, {(2,2), (3,2), (4,2)})
+#we use global variables if certain ships or fleets are used in multiple test functions
+s2 = (5, 7, False, 3, set())
+s3 = (3, 0, True, 4, set())
+s4 = (9, 9, True, 1, set())
+s5 = (5, 3, True, 2, set())
+s6 = (1, 8, True, 1, set())
 
+    
 def test_ship_type1():
-    #add at least one test for ship_type by the deadline of session 7 assignment
-     ship1 = (8,4,True,4)
-     assert ship_type1(ship1) == 'battleship'
-    #provide at least five tests in total for ship_type by the project submission deadline
+    assert ship_type(s1) == "Cruiser"
+    assert ship_type(s2) == "Cruiser"
+    assert ship_type(s3) == "Battleship"
+    assert ship_type(s4) == "Submarine"
+    assert ship_type(s5) == "Destroyer"
+
+def test_place_ship_at1():
+    f = []
+    place_ship_at(s2[0],s2[1],s2[2],s2[3],f)
+    expected = [s2]
+    assert expected == f
+    place_ship_at(s3[0],s3[1],s3[2],s3[3],f)
+    expected = [s2,s3]
+    assert expected == f
+    place_ship_at(s4[0],s4[1],s4[2],s4[3],f)
+    expected = [s2,s3,s4]
+    assert expected == f
+    place_ship_at(s5[0],s5[1],s5[2],s5[3],f)
+    expected = [s2,s3,s4,s5]
+    assert expected == f
+    place_ship_at(s6[0],s6[1],s6[2],s6[3],f)
+    expected = [s2,s3,s4,s5,s6]
+    assert expected == f
+    print(f)
+
+fleet = [s3,s6]
 
 def test_is_open_sea1():
     #add at least one test for open_sea by the deadline of session 7 assignment
-    fleet = [(4,1,True,4),(7,0,False,3),(9,0,True,1),(9,7,True,1),(9,9,True,1),(8,4,True,3),
-             (0,0,False,2),(2,6,False,2),(9,0,True,2),(5,3, True,1)]
-    assert is_open_sea1(5,6,fleet) == True
-          
-    #provide at least five tests in total for open_sea by the project submission deadline
+    '''fleet = [(4,1,True,4),(7,0,False,3),(9,0,True,1),(9,7,True,1),(9,9,True,1),(8,4,True,3),
+             (0,0,False,2),(2,6,False,2),(9,0,True,2),(5,3, True,1)]'''
+    assert is_open_sea(s5[0],s5[1],fleet) == True
+    assert is_open_sea(s4[0],s4[1],fleet) == True
+    assert is_open_sea(s2[0],s2[1],fleet) == True
+    assert is_open_sea(s1[0],s1[1],fleet) == False
+    assert is_open_sea(0,9,fleet) == False
 
 def test_ok_to_place_ship_at1():   
-        #add at least one test for ok_to_place_ship_at by the deadline of session 7 assignment
-        fleet = [(4,1,True,4),(7,0,False,3),(9,0,True,1),(9,7,True,1),(9,9,True,1),(8,4,True,3),
-                 (0,0,False,2),(2,6,False,2),(9,0,True,2),(5,3, True,1)]
-        assert ok_to_place_ship_at(5,3,True,1,fleet) == True
-        #provide at least five tests in total for ok_to_place_ship_at by the project submission deadline
+    assert ok_to_place_ship_at(s5[0],s5[1],s5[2],s5[3],fleet) == True
+    assert ok_to_place_ship_at(s4[0],s4[1],s4[2],s4[3],fleet) == True
+    assert ok_to_place_ship_at(s2[0],s2[1],s2[2],s2[3],fleet) == True
+    assert ok_to_place_ship_at(s1[0],s1[1],s1[2],s1[3],fleet) == False
+    assert ok_to_place_ship_at(0,9,True,2,fleet) == False
 
-def test_place_ship_at1():
-    #add at least one test for place_ship_at by the deadline of session 7 assignment
-          fleet2 = [(4,1,True,4),(7,0,False,3),(9,0,True,1),(9,7,True,1),(9,9,True,1),(8,4,True,3),
-                     (0,0,False,2),(2,6,False,2),(9,0,True,2)]
-          fleet1 = [(4,1,True,4),(7,0,False,3),(9,0,True,1),(9,7,True,1),(9,9,True,1),(8,4,True,3),
-                     (0,0,False,2),(2,6,False,2),(9,0,True,2),(5,3,True,1)]
-          assert place_ship_at1(5,3,True,1,fleet2) == fleet1
-    #provide at least five tests in total for place_ship_at by the project submission deadline
-
+'''
+def test_is_sunk1():
+    assert is_sunk(s1) == True
+    
 def test_check_if_hits1():
     #add at least one test for check_if_hits by the deadline of session 7 assignment
     fleet = [(4,1,True,4),(7,0,False,3),(9,0,True,1),(9,7,True,1),(9,9,True,1),(8,4,True,3),
@@ -68,3 +84,4 @@ def test_are_unsunk_ships_left1():
     assert are_unsunk_ships_left1(fleet) == True
     #provide at least five tests in total for are_unsunk_ships_left by the project submission deadline
     
+'''
